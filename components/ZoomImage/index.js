4a0d7af189097526b2f66a5ef0f867463d7295a7
Michelle Lim <elle@mmmanyfold.com>
2023-06-10 18:21:44 -0400

    contain zoom image to window dimensions

--- components/ZoomImage/index.js
+++ components/ZoomImage/index.js
@@ -1,38 +1,59 @@
 "use client";
-import React from "react";
+import { useState, useEffect } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
-import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
-import InnerImageZoom from "react-inner-image-zoom";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import X from "../../icon/X";
 import "./index.scss";
 
-const ZoomImage = ({ src, alt, fullWidth }) => (
-  <Dialog.Root>
-    <Dialog.Trigger asChild>
-      <img
-        className="zoom-trigger"
-        src={src}
-        style={fullWidth ? { width: "100%" } : null}
-        alt={alt}
-      />
-    </Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay className="zoom-overlay" />
-      <Dialog.Content className="zoom-content">
-        <Dialog.Close asChild>
-          <div className="close-container">
-            <X />
-          </div>
-        </Dialog.Close>
-        <InnerImageZoom
+const ZoomImage = ({ src, alt, fullWidth }) => {
+  const [contentHeight, setContentHeight] = useState(window.innerHeight - 118);
+
+  useEffect(() => {
+    function handleResize() {
+      setContentHeight(window.innerHeight - 118);
+    }
+    window.addEventListener("resize", handleResize);
+
+    return (_) => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <img
+          className="zoom-trigger"
           src={src}
-          zoomSrc={src}
-          hideCloseButton={true}
-          hideHint={true}
+          style={fullWidth ? { width: "100%" } : null}
+          alt={alt}
         />
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
-);
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="zoom-overlay" />
+        <Dialog.Content className="zoom-content">
+          <Dialog.Close asChild>
+            <div className="close-container">
+              <X />
+            </div>
+          </Dialog.Close>
+          <div class="zoom-image-container">
+            <TransformWrapper>
+              <TransformComponent
+                wrapperStyle={{
+                  maxHeight: `${contentHeight}px`,
+                  maxWidth: "100%",
+                  margin: "auto",
+                }}
+              >
+                <img src={src} alt={alt} />
+              </TransformComponent>
+            </TransformWrapper>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
 
 export default ZoomImage;
